0110857f401b6fa2ffd3ae1b571dc71b1ef2e1a4
piratejas <piratejas@gmail.com>
2023-06-15 10:41:03 +0100

    trading_game basic complete

--- trading_game.py
+++ trading_game.py
@@ -11,3 +11,23 @@
 
     # Bonus: replace 2 marbles, 1 green with 1 black (10x winner) marble and 1 red with 1 white (5x loser marble)
 
+from random import choice as draw
+
+def marble_game():
+    bag = "GGGGGGRRRR"
+    purse = 1000
+    while purse > 500:
+        bet = int(input("How much would you like to bet on this round?"))
+        marble = draw(bag)
+        result = ''
+        if marble == 'G':
+            purse += bet
+            result = "win"
+        elif marble == 'R':
+            purse -= bet
+            result = "lose"
+        msg = f"You drew a %s marble. You {result} £{str(bet)}! Press enter to continue." %("green" if marble == "G" else "red")
+        input(msg)
+    input("You don't have enough money left to play. Game over!")
+
+marble_game()
